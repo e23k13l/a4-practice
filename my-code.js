d37d9662615aa98a2ea2665290ef1002bab5217e
e23k13l <ezekielcuevas@ymail.com>
2016-09-26 13:36:32 -0600

    Add files via upload

---
+++ my-code.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("p").hide();
+	$("#pinhead").hide();
+	$("h1").click(function() {
+
+		$(this).next().fadeToggle(1000);
+
+	});
+
+});
+
+/* start of jq02 how to use buttons*/
+
+$("#testbutton").click(funtion()	{
+	
+		$("jq02").css("background-color","FF0000");]
+		$("strong").css("background-color","#ff33cc");
+	});	
+});
